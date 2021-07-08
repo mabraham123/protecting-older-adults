a16c367531862339e6b300726055e6c4cfaf61a4
mabraham123 <33029013+mabraham123@users.noreply.github.com>
2021-07-08 19:09:55 +0100

    Disabled new password display

--- src/Components/NewPassword/newpassword.js
+++ src/Components/NewPassword/newpassword.js
@@ -6,8 +6,8 @@ const newpassword = ({password}) =>{
     
     return(
         <>
-        <p><b>- Suggested new password</b> <Popup content='Password was randomly genereated' trigger={<Icon name='info circle' />} />: {password}</p>
-        <p>(Make sure to save the password, once you leave the page it will be gone forever)</p>
+        {/* <p><b>- Suggested new password</b> <Popup content='Password was randomly genereated' trigger={<Icon name='info circle' />} />: {password}</p>
+        <p>(Make sure to save the password, once you leave the page it will be gone forever)</p> */}
         <Link to='/tools'>See more password generation options</Link>
         </>
     )
