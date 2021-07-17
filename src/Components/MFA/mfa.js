8ab4f49139ec14fefcda92e6d2173f81ba5c1ba0
mabraham123 <33029013+mabraham123@users.noreply.github.com>
2021-07-17 22:04:10 +0100

    Update mfa.js

--- src/Components/MFA/mfa.js
+++ src/Components/MFA/mfa.js
@@ -24,7 +24,7 @@ const mfa = ({accounts,solution,MFA}) =>{
                     </Card.Meta>
                     <Card.Description>
                         <h3>Multi-Factor Authentication</h3>
-                        <p>These accounts use Multi-Factor Authentication (MFA), this is when you need more than just a password to access an account. If a hacker cracks your password then you have layer of security to stop them getting into your account. Multi-Factor usually means at least 2 (sometimes referred to as two factor authentication) or more of these three categories: ‘Something you know’ (a password), ‘Something you have’ (a phone or book etc.) and ‘Something you are’ (Thumbprint, Face Authentication etc.). </p>
+                        <p>These accounts use Multi-Factor Authentication (MFA), this is when you need more than just a password to access an account. If a hacker cracks your password then you have layer of security to stop them getting into your account. Multi-Factor usually means at least 2 (sometimes referred to as two factor authentication) or more of these three categories: ‘Something you know’ (a password), ‘Something you have’ (a phone) and ‘Something you are’ (Thumbprint, Face Authentication etc.). </p>
                         
                         <h3 className="green">{accounts.length} account(s) at protected:</h3>
                         {render_at__accounts_at_risk()}   
@@ -46,7 +46,7 @@ const mfa = ({accounts,solution,MFA}) =>{
                     </Card.Meta>
                     <Card.Description>
                         <h3>Multi-Factor Authentication</h3>
-                        <p>These accounts doesn’t use Multi-Factor Authentication (MFA), this is when you need more than just a password to access an account. If a hacker cracks your password then there is another layer of security to stop them getting into your account. Multi-Factor usually means at least 2 or more of these three categories: ‘Something you know’ (a password), ‘Something you have’ (a phone or book etc.) and ‘Something you are’ (Thumbprint, Face Authentication etc.). </p>
+                        <p>These accounts doesn’t use Multi-Factor Authentication (MFA), this is when you need more than just a password to access an account. If a hacker cracks your password then there is another layer of security to stop them getting into your account. Multi-Factor usually means at least 2 or more of these three categories: ‘Something you know’ (a password), ‘Something you have’ (a phone) and ‘Something you are’ (Thumbprint, Face Authentication etc.). </p>
                         <h3 className="red">{accounts.length} account(s) at risk:</h3>
                         {render_at__accounts_at_risk()}   
                         <h3>Recommendation(s)</h3>
